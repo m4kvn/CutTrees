e87537ff9ac2101049c2b6049f11008d779cdf3c
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-01-27 20:52:43 +0900

    Gradle Init by IDEA

---
+++ src/main/kotlin/com/masahirosaito/spigot/cuttrees/CutTrees.kt
@@ -0,0 +1,15 @@
+package com.masahirosaito.spigot.cuttrees
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class CutTrees : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
