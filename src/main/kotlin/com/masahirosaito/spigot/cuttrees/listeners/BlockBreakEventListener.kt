ff59833934c11b3685b051f905d11b7eaabc9fa4
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-07 07:24:41 +0900

    ブロック破壊イベント処理

--- src/main/kotlin/com/masahirosaito/spigot/cuttrees/listeners/BlockBreakEventListener.kt
+++ src/main/kotlin/com/masahirosaito/spigot/cuttrees/listeners/BlockBreakEventListener.kt
@@ -4,10 +4,11 @@ import com.masahirosaito.spigot.cuttrees.CutTrees
 import com.masahirosaito.spigot.cuttrees.CutTreesAbstract
 import com.masahirosaito.spigot.cuttrees.events.NoReduceTreeBreakEvent
 import com.masahirosaito.spigot.cuttrees.events.ReduceTreeBreakEvent
-import com.masahirosaito.spigot.cuttrees.utils.call
-import com.masahirosaito.spigot.cuttrees.utils.isCreativeMode
-import com.masahirosaito.spigot.cuttrees.utils.itemInMainHand
+import com.masahirosaito.spigot.cuttrees.trees.JungleTree
+import com.masahirosaito.spigot.cuttrees.trees.OakTree
+import com.masahirosaito.spigot.cuttrees.utils.*
 import net.md_5.bungee.api.ChatColor
+import org.bukkit.TreeSpecies
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -17,39 +18,49 @@ class BlockBreakEventListener(plugin: CutTrees) : CutTreesAbstract(plugin), List
 
     @EventHandler(priority = EventPriority.MONITOR)
     fun onBlockBreak(event: BlockBreakEvent) {
-        if (event.isCancelled) return
-        if (isInValid(event)) return antiBlockManager.remove(event.block)
+        if (!event.block.isTree()) return
 
-        debugMsg(event)
-
-        if (isNotReduceDurability(event)) {
-            NoReduceTreeBreakEvent(event, plugin).call(plugin)
-        } else {
-            ReduceTreeBreakEvent(event, plugin).call(plugin)
+        val tree = when (event.block.asTree().species) {
+            TreeSpecies.GENERIC -> OakTree(event.block)
+            TreeSpecies.JUNGLE -> JungleTree(event.block)
+            else -> return
         }
+
+        tree.breakTree()
+
+//        if (event.isCancelled) return
+//        if (isInValid(event)) return antiBlockManager.remove(event.block)
+//
+//        debugMsg(event)
+//
+//        if (isNotReduceDurability(event)) {
+//            NoReduceTreeBreakEvent(event, plugin).call(plugin)
+//        } else {
+//            ReduceTreeBreakEvent(event, plugin).call(plugin)
+//        }
     }
 
-    private fun debugMsg(event: BlockBreakEvent) {
-        messenger.debug(buildString {
-            append("${ChatColor.GOLD}")
-            append("[Valid Event] $event")
-            append("${ChatColor.RESET}")
-        })
-    }
-
-    private fun isNotReduceDurability(event: BlockBreakEvent): Boolean = when {
-        (event.player.isCreativeMode() && !configs.onCreativeDurabilityReduce) -> true
-        (event.player.itemInMainHand().itemMeta.spigot().isUnbreakable) -> true
-        else -> false
-    }
-
-    private fun isInValid(event: BlockBreakEvent): Boolean {
-        return when {
-            !configs.isValidBlock(event.block) -> true.apply { messenger.debug("Block is InValid") }
-            !configs.isNotAnti(event.block) -> true.apply { messenger.debug("Block is AntiBlock") }
-            !configs.isValidTool(event.player.itemInMainHand()) -> true.apply { messenger.debug("Tool is InValid") }
-            !configs.isSneaking(event.player) -> true.apply { messenger.debug("Player is not Sneaking") }
-            else -> false
-        }
-    }
+//    private fun debugMsg(event: BlockBreakEvent) {
+//        messenger.debug(buildString {
+//            append("${ChatColor.GOLD}")
+//            append("[Valid Event] $event")
+//            append("${ChatColor.RESET}")
+//        })
+//    }
+//
+//    private fun isNotReduceDurability(event: BlockBreakEvent): Boolean = when {
+//        (event.player.isCreativeMode() && !configs.onCreativeDurabilityReduce) -> true
+//        (event.player.itemInMainHand().itemMeta.spigot().isUnbreakable) -> true
+//        else -> false
+//    }
+//
+//    private fun isInValid(event: BlockBreakEvent): Boolean {
+//        return when {
+//            !configs.isValidBlock(event.block) -> true.apply { messenger.debug("Block is InValid") }
+//            !configs.isNotAnti(event.block) -> true.apply { messenger.debug("Block is AntiBlock") }
+//            !configs.isValidTool(event.player.itemInMainHand()) -> true.apply { messenger.debug("Tool is InValid") }
+//            !configs.isSneaking(event.player) -> true.apply { messenger.debug("Player is not Sneaking") }
+//            else -> false
+//        }
+//    }
 }
