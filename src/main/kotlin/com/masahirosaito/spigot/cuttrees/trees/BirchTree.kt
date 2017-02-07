bc0200b1828930ddce5ae67b5e50c1e5b217f43a
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-02-07 16:06:48 +0900

    白樺の木を追加

---
+++ src/main/kotlin/com/masahirosaito/spigot/cuttrees/trees/BirchTree.kt
@@ -0,0 +1,29 @@
+package com.masahirosaito.spigot.cuttrees.trees
+
+import com.masahirosaito.spigot.cuttrees.utils.asLeaves
+import com.masahirosaito.spigot.cuttrees.utils.asTree
+import com.masahirosaito.spigot.cuttrees.utils.isLeaves
+import com.masahirosaito.spigot.cuttrees.utils.isTree
+import org.bukkit.TreeSpecies
+import org.bukkit.block.Block
+
+class BirchTree(block: Block) : BaseTree(block) {
+    val MAX_HEIGHT = 6
+    val MIN_HEIGHT = 4
+
+    override fun leavesRange(): Int = 4
+
+    override fun maxLogBranch(): Int = 0
+
+    override fun isValid(blocks: MutableSet<Block>): Boolean {
+        return (getTop(blocks).y - getBottom(blocks).y + 1).let { MIN_HEIGHT <= it && it <= MAX_HEIGHT }
+    }
+
+    override fun isSame(block: Block): Boolean {
+        return if (block.isTree()) block.asTree().species == TreeSpecies.BIRCH else false
+    }
+
+    override fun isSameLeaves(block: Block): Boolean {
+        return if (block.isLeaves()) block.asLeaves().species == TreeSpecies.BIRCH else false
+    }
+}
